4442f5ff16e2dec44583c11c6c3623ed956aa6e2
calyeefornia96 <calvinyee@hotmail.com>
2016-12-14 16:01:39 +0800

    added flex box project

---
+++ Flex Panel Gallery/script.js
@@ -0,0 +1,15 @@
+const panels = $('.panel');
+
+console.log(panels);
+function toggleOpen(){
+	$(this).toggleClass('open');
+}
+
+function toggleActive(e){
+	if(e.originalEvent.propertyName.includes('flex')){
+		$(this).toggleClass('open-active');
+	}
+}
+
+panels.each(function(){$(this).on('click', toggleOpen);});
+panels.each(function(){$(this).on('transitionend', toggleActive);});
